639cb84a3a92fb50c194c755bd8554786eccc672
sean <sean@hella.rip>
2017-11-24 14:34:04 -0600

    Add domain blacklist using sync (global, cross-broser) storage

--- README.md
+++ README.md
@@ -22,6 +22,8 @@ The extension quickly checks for the presence of a few common `div` classes and 
 
 It preserves the formatting, images, and styling of the original recipe card as closely as possible; it doesn't try to "sanitize" or reformat the recipe.
 
+Buttons are added to the recipe card to control the extension. You can choose to close the popup or blacklist the site entirely so the extension will no longer try to call out recipes on the current domain.
+
 
 
 ### Developing & Contributing
--- src/js/main.js
+++ src/js/main.js
@@ -7,19 +7,39 @@ recipe_selectors = [
 	'.innerrecipe',
 ]
 
+function hidePopup(){
+	$('#_rf_highlight').fadeOut();
+}
 
-recipe_selectors.every(function(s){
-	$r = $(s);
-	if ($r.length === 1){
-		$r.clone().attr('id', '_rf_highlight').prependTo('body').append(`
-			<div id="_rf_header">
-				<button id="_rf_closebtn">close recipe filter</button>
-			</div>
-		`).fadeIn(500);
-		$('#_rf_closebtn').click(function(b){
-			$('#_rf_highlight').fadeOut();
-		});
-		return false;
+function showPopup(){
+	recipe_selectors.every(function(s){
+		$r = $(s);
+		if ($r.length === 1){
+			$r.clone().attr('id', '_rf_highlight').prependTo('body').append(`
+				<div id="_rf_header">
+					<button id="_rf_closebtn">close recipe</button> 
+					<button id="_rf_disablebtn">disable on this site</button> 
+				</div>
+			`).fadeIn(500);
+
+			$('#_rf_closebtn').click(function(b){
+				hidePopup();
+			});
+			
+			$('#_rf_disablebtn').click(function(b){
+				chrome.storage.sync.set({[document.location.hostname]: true}, function(){
+					hidePopup();
+				});
+			});
+			return false;	// it worked, stop iterating through recipe_selectors
+		}
+		return true;
+	});
+}
+
+// check the blacklist to see if we should run on this site
+chrome.storage.sync.get(document.location.hostname, function(items) {
+	if (!(document.location.hostname in items)) {
+		showPopup();
 	}
-	return true;
 });
