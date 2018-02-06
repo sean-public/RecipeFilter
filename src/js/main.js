9c690655f14b84eef04d260eaf4a74693dde8755
sean <sean@hella.rip>
2018-02-05 22:26:12 -0600

    Enhance button bar added to container styling and colorize our button in the browser bar via manifest.

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ recipe_selectors = [
 	'div[itemtype="https://schema.org/Recipe"]',
 ]
 
+
 function hidePopup(){
 	$('#_rf_highlight').fadeOut();
 }
@@ -17,18 +18,35 @@ function showPopup(){
 	recipe_selectors.every(function(s){
 		$r = $(s);
 		if ($r.length === 1){
+			// clone the matched element and add some control buttons
 			$r.clone().attr('id', '_rf_highlight').prependTo('body').append(`
 				<div id="_rf_header">
-					<button id="_rf_closebtn">close recipe</button> 
-					<button id="_rf_disablebtn">disable on this site</button> 
+					<button id="_rf_closebtn" class="_rfbtn">close recipe</button>
+					RecipeFilter
+					<button id="_rf_disablebtn" class="_rfbtn">disable on this site</button>
 				</div>
 			`).fadeIn(500);
 
+			// handle the two new buttons we attached to the popup
 			$('#_rf_closebtn').click(hidePopup);
 			$('#_rf_disablebtn').click(function(b){
 				chrome.storage.sync.set({[document.location.hostname]: true}, hidePopup);
 			});
-			return false;	// it worked, stop iterating through recipe_selectors
+
+			// add an event listener for clicking outside the recipe to close it
+			$(document).mouseup(function(e) {
+				var container = $('#_rf_highlight');
+				if (!container.is(e.target) && container.has(e.target).length === 0) 
+				{
+				    hidePopup();
+				    $(document).unbind('mouseup');
+				}
+			});
+
+			// scroll to top in case they hit refresh while lower in page
+			$(window).scrollTop(0);
+			// it worked, stop iterating through recipe_selectors
+			return false;
 		}
 		return true;
 	});
@@ -40,4 +58,3 @@ chrome.storage.sync.get(document.location.hostname, function(items) {
 		showPopup();
 	}
 });
-
