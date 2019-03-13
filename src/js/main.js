e4171cb37c7bf5c9c44c34e835ee76dc96ec7893
curtis mcallister <curtis.s.mcallister@gmail.com>
2019-03-13 15:41:06 -0700

    Add selector for smittenkitchen, Add support for recipes that are more than one node

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-recipe_selectors = [
+const recipe_selectors = [
 	'.recipe-callout',
 	'.tasty-recipes',
 	'.easyrecipe',
@@ -9,8 +9,16 @@ recipe_selectors = [
 	'.simple-recipe-pro',
 	'div[itemtype="http://schema.org/Recipe"]',
 	'div[itemtype="https://schema.org/Recipe"]',
+	'.entry-content > :not(.smittenkitchen-print-hide)' // smittenkitchen.com
 ]
 
+const controls = `
+	<div id="_rf_header">
+		<button id="_rf_closebtn" class="_rfbtn">close recipe</button>
+		RecipeFilter
+		<button id="_rf_disablebtn" class="_rfbtn">disable on this site</button>
+	</div>
+`
 
 function hidePopup(){
 	$('#_rf_highlight').fadeOut();
@@ -21,13 +29,7 @@ function showPopup(){
 		$r = $(s);
 		if ($r.length === 1){
 			// clone the matched element and add some control buttons
-			$r.clone().attr('id', '_rf_highlight').prependTo('body').append(`
-				<div id="_rf_header">
-					<button id="_rf_closebtn" class="_rfbtn">close recipe</button>
-					RecipeFilter
-					<button id="_rf_disablebtn" class="_rfbtn">disable on this site</button>
-				</div>
-			`).fadeIn(500);
+			$r.clone().attr('id', '_rf_highlight').prependTo('body').append(controls).fadeIn(500);
 
 			// handle the two new buttons we attached to the popup
 			$('#_rf_closebtn').click(hidePopup);
@@ -42,6 +44,36 @@ function showPopup(){
 				{
 				    hidePopup();
 				    $(document).unbind('mouseup');
+				}
+			});
+
+			// scroll to top in case they hit refresh while lower in page
+			$(window).scrollTop(0);
+			// it worked, stop iterating through recipe_selectors
+			return false;
+
+		} if ($r.length > 1) {
+
+			$(document.createElement('div'))
+				.attr('id', '_rf_highlight')
+				.prependTo('body')
+				.append($r.clone())
+				.append(controls)
+				.fadeIn(500)
+
+			// handle the two new buttons we attached to the popup
+			$('#_rf_closebtn').click(hidePopup);
+			$('#_rf_disablebtn').click(function(b){
+				chrome.storage.sync.set({[document.location.hostname]: true}, hidePopup);
+			});
+
+			// add an event listener for clicking outside the recipe to close it
+			$(document).mouseup(function(e) {
+				var container = $('#_rf_highlight');
+				if (!container.is(e.target) && container.has(e.target).length === 0)
+				{
+					hidePopup();
+					$(document).unbind('mouseup');
 				}
 			});
 
