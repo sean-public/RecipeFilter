463498f119c1fbfb33954c032d5ca9b8988c89f1
Jonathan Campbell <jon@healsdata.com>
2024-09-08 17:26:44 -0400

    fix: don't try foreach an empty array

--- src/js/options.js
+++ src/js/options.js
@@ -8,13 +8,18 @@ chrome.storage.sync.get(null, function(data){
 	let rowMarkup = '';
 	let selectors = [];
 
-	for (site in data) {
+	for (let site in data) {
 		rowMarkup += `<tr>
 			<td id="${site}" class="trash-cell" style="cursor:pointer;">&#9447;</td>
 			<td>${site}</td>
 		</tr>`;
 		selectors.push(`[id="${site}"]`);
 	}
+
+	if (selectors.length === 0) {
+		return;
+	}
+
 	document.querySelector('#blacklist tbody').innerHTML = rowMarkup;
 	document.querySelectorAll(selectors.join(',')).forEach((td) => {
 		td.addEventListener('click', remove);
